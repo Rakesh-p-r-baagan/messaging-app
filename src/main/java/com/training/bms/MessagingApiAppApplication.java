727d962b9f0f847fbf9ddaad219d9cd941804b7c
rakeshpr <baagan8@gmail.com>
2024-07-12 14:54:31 +0530

    new changes with a test

--- src/main/java/com/training/bms/MessagingApiAppApplication.java
+++ src/main/java/com/training/bms/MessagingApiAppApplication.java
@@ -2,14 +2,18 @@ package com.training.bms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MessagingApiAppApplication {
 	
-	public void printHi()
-	{
-		System.out.println("hello");
-	}
+	 @GetMapping("/greeting")
+	 
+	    public String greet() {
+	        return "Hello, Jenkins!";
+	    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingApiAppApplication.class, args);
