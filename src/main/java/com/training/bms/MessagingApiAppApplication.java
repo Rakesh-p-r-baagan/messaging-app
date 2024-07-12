00dc055d16e8902337a69fbdc2bbe6af0389b90b
rakeshpr <baagan8@gmail.com>
2024-07-12 14:28:52 +0530

    fail commit

--- src/main/java/com/training/bms/MessagingApiAppApplication.java
+++ src/main/java/com/training/bms/MessagingApiAppApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MessagingApiAppApplication {
+	
+	public void printHi()
+	{
+		System.out.println("hello");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingApiAppApplication.class, args);
--- src/test/java/com/training/bms/MessagingApiAppApplicationTests.java
+++ src/test/java/com/training/bms/MessagingApiAppApplicationTests.java
@@ -1,5 +1,7 @@
 package com.training.bms;
 
+import static org.assertj.core.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,12 @@ class MessagingApiAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+//		assertEquals(200,200);
 	}
+	
+    @Test
+    public void intentionallyFailingTest() {
+        fail("This testntionally fails.");
+    }
 
 }
